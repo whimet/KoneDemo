65fe3aee7f4b070b9d836683775236834206bd7c
Li Yanhui <whimet@gmail.com>
2012-08-30 20:10:47 +0800

    change port

--- src/main/java/demo/JettyServer.java
+++ src/main/java/demo/JettyServer.java
@@ -6,7 +6,7 @@ import org.mortbay.jetty.webapp.WebAppContext;
 public class JettyServer {
 
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+        Server server = new Server(8090);
         WebAppContext webapp = new WebAppContext("src/main/webapp", "/kone");
         server.addHandler(webapp);
         server.start();
